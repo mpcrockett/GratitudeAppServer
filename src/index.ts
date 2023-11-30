5c042d9581a06f112555db966124b5cfaef42364
mpcrockett <mpcrockett+github@gmail.com>
2023-11-30 14:59:41 -0500

    prisma set up

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
 import express, { Request, Response } from 'express';
+import prisma from './connectors/db';
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.get('/', async (req, res) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: 1
+    }
+  })
+  res.status(200).send(user);
+});
+
+const server = app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+export default server;
